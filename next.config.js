881d8c87fba707a477d5d75c9c7b62de68b5532f
Aruseli <llingveri@gmail.com>
2021-07-06 06:19:58 +0000

    edit next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: isProd ? '/scrollrelativity' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/aruseli/scrollrelativity/gh-pages/' : '',
+};
